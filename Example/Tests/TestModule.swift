62134d79457d6fdee325ae89d6a9b2967e864632
Martin Eberl <martin.eberl@styria.com>
2017-07-13 07:52:46 +0200

    Add unit tests

--- Example/Tests/TestModule.swift
+++ Example/Tests/TestModule.swift
@@ -13,11 +13,27 @@ protocol StringProvidable {
     var test: String { get }
 }
 
-final class TestModule: Module, StringProvidable {
+protocol SingletonFooProvidable {
+    var singletonFoo: Foo { get }
+}
+
+protocol FooProvidable {
+    var foo: Foo { get }
+}
+
+final class TestModule: Module, StringProvidable, SingletonFooProvidable, FooProvidable {
     weak var delegate: ModuleDelegate?
     static let defaultText = "Hello World"
     
     var test: String {
         return TestModule.defaultText
+    }
+    
+    lazy var singletonFoo: Foo = {
+       return Foo()
+    }()
+    
+    var foo: Foo {
+        return Foo()
     }
 }
